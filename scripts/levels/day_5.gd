d0c37000367966843637615cbec84d1791d9a03e
Pfftz <Kalixpfft+github@protonmail.com>
2025-08-04 01:15:37 +0700

    Add video player functionality and integrate intro videos for Day 1 and Day 5

--- scripts/levels/day_5.gd
+++ scripts/levels/day_5.gd
@@ -1,15 +1,19 @@
 extends Node3D
 
+@onready var video_player = $VideoPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Check if monolog has already been played for this day
 	if not GameState.is_monolog_played(5):
-		# If monolog hasn't been played, start it and mark as played
-		GameState.mark_monolog_played(5)
-		Dialogic.start("monolog/day5")
+		# Play Day 5 video first, then monolog
+		play_day5_video()
 
+func play_day5_video():
+	if video_player:
+		video_player.play_video("res://asset/videos/Day5_intro.ogv", _on_day5_video_finished)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_day5_video_finished():
+	# After Day 5 video, play monolog and mark as played
+	GameState.mark_monolog_played(5)
+	Dialogic.start("monolog/day5")
